f45a1f987e694f1cb914dc731b04dc58aa0cbc3d
weierstrassmtest <97811425+weierstrassmtest@users.noreply.github.com>
2022-01-25 18:25:07 -0700

    Update sandbox.py

--- code/python/column_reordering/sandbox.py
+++ code/python/column_reordering/sandbox.py
@@ -1,1 +1,8 @@
-print("hello world")
+def Reorder_Column(dataframe, col_to_move, ref_col):
+  reordered_frame = dataframe.copy(deep=True)
+  all_columns = []
+  # Create List of all columns in original dataframe
+  for column in reordered_frame.columns:
+    all_columns = all_columns.append(column)
+  
+  return reordered_frame
