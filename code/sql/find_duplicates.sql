8b6eeb8e559dff98a58ee6ff4347f32606c20e6b
weierstrassmtest <97811425+weierstrassmtest@users.noreply.github.com>
2022-02-15 15:24:10 -0700

    Update find_duplicates.sql

--- code/sql/find_duplicates.sql
+++ code/sql/find_duplicates.sql
@@ -1,1 +1,1 @@
-SELECT duped_field, COUNT(*) FROM "sample"."db" GROUP BY duped_field HAVING COUNT(*) > 1;
+SELECT duped_field, COUNT(*) FROM "sample_db"."table" GROUP BY duped_field HAVING COUNT(*) > 1;
