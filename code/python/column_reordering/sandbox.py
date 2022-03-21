f13d41d28f86e102613f08b9e22608125d8aba62
weierstrassmtest <97811425+weierstrassmtest@users.noreply.github.com>
2022-03-21 09:34:38 -0600

    Update sandbox.py

--- code/python/column_reordering/sandbox.py
+++ code/python/column_reordering/sandbox.py
@@ -6,3 +6,8 @@ def Reorder_Column(dataframe, col_to_move, ref_col):
     all_columns = all_columns.append(column)
   
   return reordered_frame
+
+# Re-order Columns
+# cols = df_ss2.columns.tolist()
+# cols = cols[-1:] + cols[:-1]
+# df_ss2 = df_ss2[cols]
