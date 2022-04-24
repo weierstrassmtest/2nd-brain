634e39b4be806d54e9f1473738fb5a38b12e9bd8
weierstrassmtest <97811425+weierstrassmtest@users.noreply.github.com>
2022-04-24 15:57:27 -0600

    Update sandbox.py

--- code/python/column_reordering/sandbox.py
+++ code/python/column_reordering/sandbox.py
@@ -1,20 +1,13 @@
-def Reorder_Columns(input_frame, ordered_col_list, action_code, ref_col):
-  # ACTION CODES
-  # 0 = Move to beginning of DF
-  # 1 = Move to end of DF
-  reordered_frame = dataframe.copy(deep=True)
-  all_columns = reordered_frame.columns
-  other_columns = []
-  for item in all_columns:
-    if item not in ordered_col_list:
-      other_columns.append(item)
-  # Create List of all columns in original dataframe
-  for column in reordered_frame.columns:
-    all_columns = all_columns.append(column)
-  
-  return reordered_frame
+# Can be spark df OR pandas df
 
-# Re-order Columns
-# cols = df_ss2.columns.tolist()
-# cols = cols[-1:] + cols[:-1]
-# df_ss2 = df_ss2[cols]
+def Reorder_Columns(input_spark_frame, ordered_col_list):
+    all_columns = input_spark_frame.columns
+    other_columns = []
+    for item in all_columns:
+        if item not in ordered_col_list:
+          other_columns.append(item)
+        else:
+            pass
+    ordered_col_list.extend(other_columns)
+    reordered_frame = input_spark_frame.select(ordered_col_list)
+    return reordered_frame
